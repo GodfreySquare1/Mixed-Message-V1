1447b0a8aee26cdd5f934944f25c631b57caad24
Godfrey Cox <godfrey@square1grp.com>
2022-01-17 21:17:14 +0200

    New Script

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const message = () => {
+const userNameGenerator = () => {
     const randomString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let results = '';
+    let randomUserName = '';
     // console.log(randomString.length);
     for (let i = 0; i < 15; i++) {
-        results += randomString.charAt(Math.floor(Math.random() * randomString.length))
+        randomUserName += randomString.charAt(Math.floor(Math.random() * randomString.length))
     }
 
-    return console.log(`Hello user: ${results}, thank you for starting up the program!`);
+    return randomUserName;
 
 }
 
-console.log(message());
+const randWelcomeMessage = () => {
+    const strArray = [['You did it!'], ['Welcome to the team'], 'No No No!!!']
+    let results1 = [];
+    for (let i = 0; i < strArray.length; i++) {
+        results1 = strArray.Math.random();
+        console.log(results1);
+    };
+    return results1;
+
+};
+
+let checkUserData = (userName, text) => {
+    return {
+        username: userName,
+        userMessage: text,
+    }
+};
+
+
+console.log(message(randWelcomeMessage()));
